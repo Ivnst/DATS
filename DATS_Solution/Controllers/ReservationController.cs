a44a8bc3d1972babd531427541fa6979a81805a4
Ivnst <for_spaming@bk.ru>
2014-07-10 16:09:04 +0300

    Working on reservation (not completed)
    Reservation button on home page.
    Table header on reservation page.

---
+++ DATS_Solution/Controllers/ReservationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DATS.Controllers
+{
+    public class ReservationController : BaseController
+    {
+        //
+        // GET: /Reservation/
+
+        public ActionResult Index()
+        {
+          FillViewBag(CurrentStadium, CurrentMatch);
+          
+          return View();
+        }
+
+
+        public ActionResult Create()
+        {
+          return PartialView(new Client());
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Client client)
+        {
+          if (ModelState.IsValid)
+          {
+            return RedirectToAction("Index", "Home");
+          }
+          else
+          {
+            return View(client);
+          }
+        }
+    }
+}
--- DATS_Solution/Models/Clients.cs
+++ DATS_Solution/Models/Clients.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System;
+using System.ComponentModel;
 
 namespace DATS
 {
@@ -13,17 +14,20 @@ namespace DATS
     /// <summary>
     /// Код матча
     /// </summary>
-    [Required]
+    [Required(ErrorMessage = "Пожалуйста, укажите имя клиента!")]
+    [DisplayName("Имя клиента")]
     public string Name { get; set; }
     /// <summary>
     /// Код матча
     /// </summary>
-    [Required]
+    [Required(ErrorMessage = "Пожалуйста, укажите контактные данные клиента!")]
+    [DisplayName("Контактные данные")]
     public string Contact { get; set; }
     /// <summary>
     /// Дата создания клиента
     /// </summary>
     [Required]
+    [DisplayName("Дата бронирования")]
     public DateTime Date { get; set; }
     /// <summary>
     /// Если true - бронь по текущему клиенту активна.
