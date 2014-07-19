1a7a4cea7d7f2bd90f9354d3c3408f5deadb0189
Ivnst <for_spaming@bk.ru>
2014-07-20 01:26:18 +0300

    Added class ConfigView and methods for work with it

--- DATS_Solution/DB/Partials/SqlRep-Configs.cs
+++ DATS_Solution/DB/Partials/SqlRep-Configs.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data.Entity;
 using System.Linq;
 using System.Collections.Generic;
+using System;
 
 namespace DATS
 {
@@ -14,5 +15,66 @@ namespace DATS
     /// </summary>
     public DbSet<Config> Configs { get; set; }
 
+    /// <summary>
+    /// Получение параметров стадиона
+    /// </summary>
+    /// <param name="stadium"></param>
+    /// <returns></returns>
+    public ConfigView GetConfigView(Stadium stadium)
+    {
+      if(stadium == null) throw new ArgumentNullException("stadium");
+
+      //достаём все параметры, относящиеся к указанному стадиону
+      List<Config> configsList = Configs.Where<Config>(c => c.StadiumId == stadium.Id).ToList<Config>();
+
+      ConfigView result = new ConfigView();
+      result.StadiumId = stadium.Id;
+
+      //ищем необходимые параметры
+      foreach (Config config in configsList)
+      {
+        if (config.Name == ConfigView.RemoveReservationPeriod_ConfigName)
+          result.RemoveReservationPeriod = Convert.ToInt32(config.Val);
+
+        //если добавится новый параметр - добавить сюда
+      }
+
+      return result;
+    }
+
+
+    /// <summary>
+    /// Сохранение параметров стадиона
+    /// </summary>
+    /// <param name="configView"></param>
+    public void SetConfigView(ConfigView configView)
+    {
+      if (configView == null) throw new ArgumentNullException("configView");
+
+
+      //достаём все параметры, относящиеся к указанному стадиону
+      List<Config> configsList = Configs.Where<Config>(c => c.StadiumId == configView.StadiumId).ToList<Config>();
+
+
+      //сохраняем параметр RemoveReservationPeriod
+      Config reservationRemovePeriodConfig = configsList.Find(c => c.Name == ConfigView.RemoveReservationPeriod_ConfigName);
+      if(reservationRemovePeriodConfig == null)
+      {
+        Config newConfig = new Config();
+        newConfig.StadiumId = configView.StadiumId;
+        newConfig.Name = ConfigView.RemoveReservationPeriod_ConfigName;
+        newConfig.Val = configView.RemoveReservationPeriod.ToString();
+        Configs.Add(newConfig);
+      }
+      else
+      {
+        reservationRemovePeriodConfig.Val = configView.RemoveReservationPeriod.ToString();
+        this.Entry<Config>(reservationRemovePeriodConfig).State = EntityState.Modified;
+      }
+
+      //если добавится новый параметр - добавить сюда
+
+      SaveChanges();
+    }
   }
 }
---
+++ DATS_Solution/Models/ModelsView/ConfigView.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace DATS
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  public class ConfigView
+  {
+    /// <summary>
+    /// Имя стадиона
+    /// </summary>
+    public int StadiumId { get; set; }
+    /// <summary>
+    /// Время, за которое до начала мероприятия все забронированные места попадают в продажу (в минутах)
+    /// </summary>
+    public int RemoveReservationPeriod { get; set; }
+
+    #region <Static fields>
+    
+    /// <summary>
+    /// Название параметра в базе данных для поля RemoveReservationPeriod
+    /// </summary>
+    public static string RemoveReservationPeriod_ConfigName = "rrperiod";
+
+    #endregion
+  }
+}
