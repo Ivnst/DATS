5226f31b7ce30545d1148db96b5fb2b710871f98
sekonungr <sekonungr@gmail.com>
2014-07-23 14:22:38 +0300

    Improvement of the method validation for the field BegitAt in MatchSetting.

--- DATS_Solution/Scripts/CustomValidation/MatchValidation.js
+++ DATS_Solution/Scripts/CustomValidation/MatchValidation.js
@@ -1,7 +1,15 @@
-﻿$('#myModal form').validate({  // initialize plugin
+﻿$.validator.addMethod(
+    "vdate",
+    function (value, element) {
+         return validium();
+    },
+    "Формат должен быть dd.MM.yyyy HH:mm в диапазоне от 01.01.2000 до 31.12.2099."
+);
+
+$('#myModal form').validate({  // initialize plugin
     rules: {
         Name: { required: true, maxlength: 255 },
-        BeginsAt: { required: true },
+        BeginsAt: { vdate: true },
         Duration: { required: true, digits: true, range: [1, 2147483647] }
     },
 
@@ -16,3 +24,38 @@
     onfocusout: false
 
 });
+
+function validium() {
+    var nonError = true;
+    var errMessage1 = "", errMessage2 = "";
+    $("#begining").val(function (i, v) {
+        var fullStr = v;
+        var dateTmp = fullStr.substring(0, fullStr.indexOf(" "));
+        var timeTmp = fullStr.substring(fullStr.indexOf(" ") + 1);
+
+        var dayStr = dateTmp.substring(0, dateTmp.indexOf("."));
+        dateTmp = dateTmp.substring(dateTmp.indexOf(".") + 1);
+        var monthStr = dateTmp.substring(0, dateTmp.indexOf("."));
+        dateTmp = dateTmp.substring(dateTmp.indexOf(".") + 1);
+        var yearStr = dateTmp;
+
+        var hourStr = timeTmp.substring(0, timeTmp.indexOf(":"));
+        timeTmp = timeTmp.substring(timeTmp.indexOf(":") + 1);
+        var minuteStr = timeTmp;
+
+        var msNoZone = Date.parse(yearStr + '-' + monthStr + '-' + dayStr + 'T' + hourStr + ':' + minuteStr + ':00.000');
+
+        if (!isFinite(msNoZone)) {
+            nonError = false;
+        } else {
+            if ((msNoZone >= 946684800000) && (msNoZone <= 4102358400000)) {
+                // Ok
+            } else {
+                nonError = false;
+            }
+        }
+
+        return v;
+    });
+    return nonError;
+}
