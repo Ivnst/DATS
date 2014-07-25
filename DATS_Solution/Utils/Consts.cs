aa1def2ba3cff03424cadcf624effda86bfbfd69
Ivnst <for_spaming@bk.ru>
2014-07-25 12:44:47 +0300

    Fixed problem with saving places with wrong position in sector.

---
+++ DATS_Solution/Utils/Consts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DATS
+{
+  /// <summary>
+  /// Некоторые константы, необходимые приложению
+  /// </summary>
+  public static class Consts
+  {
+    /// <summary>
+    /// Максимальная ширина сектора
+    /// </summary>
+    public const int MaxSectorWidth = 50;
+
+    /// <summary>
+    /// Максимальная высота сектора
+    /// </summary>
+    public const int MaxSectorHeight = 25;
+  }
+}
