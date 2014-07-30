00106f096b68bd95d32de1746721891b2ac4b7c6
Tanya8 <tanya.kumar11.02@gmail.com>
2014-07-30 17:00:41 +0300

    Added a logo, implemented date and time displaying on the settings page

---
+++ DATS_Solution/Scripts/Custom/DateAndTime.js
@@ -0,0 +1,30 @@
+﻿
+    // Определение функции для отображения текущего времени
+    $().ready(function () { displayTime(); setInterval(function () { displayTime(); }, 30000); })
+   function displayTime()
+    {
+        var element = document.getElementById("Clock"); // Найти элемент с id="Clock"
+        var now = new Date(); // Получить текущее время
+        var month = now.getMonth() + 1;
+        if (month <= 9)
+        {
+            month.toLocaleString();
+            month = "0" + month;
+        }
+        var day = now.getDate();
+        var year = now.getFullYear();
+        var hours = now.getHours();
+        var minutes = now.getMinutes();
+        if (minutes <= 9)
+        {
+            minutes.toLocaleString();
+            minutes = "0" + minutes;
+        }
+       
+        element.innerHTML =  day + '.' +month + '.' + year + "      " + hours + ":" + minutes; // Отобразить его
+       
+    }         
+
+
+
+    
