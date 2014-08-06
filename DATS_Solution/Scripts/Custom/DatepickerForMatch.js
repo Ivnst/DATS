8c593098506e38039c742fa1848e1915cba391b9
sekonungr <sekonungr@gmail.com>
2014-08-06 15:21:32 +0300

    Impruvements for calendar in Matches.

---
+++ DATS_Solution/Scripts/Custom/DatepickerForMatch.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
+    $("#begining").datepicker({
+        showOn: "button",
+        buttonImage: "/Content/images/calendar.gif",
+        showButtonPanel: true,
+        firstDay: 1,
+        dateFormat: "dd.mm.yy 00:00"
+    });
+});
