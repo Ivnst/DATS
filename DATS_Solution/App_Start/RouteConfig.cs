8be3dff118a76cc98a6483f0b7c1860091470b92
sekonungr <sekonungr@gmail.com>
2014-08-11 02:22:16 +0300

    Removed custom routes, as not all parameters are passed in a format with a slash.

--- DATS_Solution/App_Start/RouteConfig.cs
+++ DATS_Solution/App_Start/RouteConfig.cs
@@ -13,13 +13,6 @@ namespace DATS
     {
       routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-      // ловим случаи ошибочного ввода URL без параметров
-      routes.MapRoute("CatchNullCreate", "{controller}/Create", new { action = "RouteError" } );
-      routes.MapRoute("CatchNullEdit", "{controller}/Edit", new { action = "RouteError" } );
-      routes.MapRoute("CatchNullDelete", "{controller}/Delete", new { action = "RouteError" } );
-      routes.MapRoute("CatchNullSave", "{controller}/Save", new { action = "RouteError" });
-      routes.MapRoute("CatchNullCopy", "{controller}/Copy", new { action = "RouteError" });
-
       // основной
       routes.MapRoute(
           name: "Default",
