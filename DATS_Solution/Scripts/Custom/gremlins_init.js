c5a5132eeb3ce5935684ba8a2fd6c2fed22c0dc2
Ivnst <for_spaming@bk.ru>
2014-08-12 14:36:57 +0300

    Fixed bundling of gremlins (an errors occured with bundling)

--- DATS_Solution/Scripts/Custom/gremlins_init.js
+++ DATS_Solution/Scripts/Custom/gremlins_init.js
@@ -4,39 +4,33 @@ var horde = null;
 $(document).ready(function () {
 
     if (document.cookie.indexOf("gremlins") != -1) {
-        horde = gremlins.createHorde();
-        horde.unleash();
-        $('#Info').html("Гремлины выпущены (Остановить - Ctrl+])..");
+        startGremlins();
     }
 
     $(document).keypress(function (e) {
         //$('#Info').html(e.which);
-        if (e.which == 91 && e.ctrlKey) {
-            horde = gremlins.createHorde();
-            horde.unleash();
-            $('#Info').html("Гремлины выпущены..");
-            if (document.cookie.indexOf("gremlins") == -1) {
-                document.cookie = "gremlins=1; path=/";
-            }
-        }
-
-        if (e.which == 93 && e.ctrlKey) {
-            if (horde != null) {
-                horde.stop();
-                horde = null;
-                $('#Info').html("Гремлины остановлены!");
-                document.cookie = "gremlins=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
-            }
-        }
-
+        if (e.which == 91 && e.ctrlKey) { startGremlins(); }
+        if (e.which == 93 && e.ctrlKey) { stopGremlins(); }
     });
 });
 
+function startGremlins() {
+    if (horde == null) {
+        horde = gremlins.createHorde()
+        horde.unleash();
+        $('#Info').html("Гремлины выпущены..(Остановка - Ctrl+])");
+    }
+    
+    if (document.cookie.indexOf("gremlins") == -1) {
+        document.cookie = "gremlins=1; path=/";
+    }
+}
 
-// возвращает cookie с именем name, если есть, если нет, то undefined
-function getCookie(name) {
-    var matches = document.cookie.match(new RegExp(
-    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-  ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
+function stopGremlins() {
+    if (horde != null) {
+        horde.stop();
+        horde = null;
+        $('#Info').html("Гремлины остановлены!");
+        document.cookie = "gremlins=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+    }
 }
